273647c8b8e288d6b0a6ed66f57753a86effb66b
creepin-coder <gldct.smurf@gmail.com>
2020-10-12 14:23:21 +0300

    fixed writing to file
    Signed-off-by: creepin-coder <gldct.smurf@gmail.com>

--- ZenCrawlerSource/spiders/Stage_One.py
+++ ZenCrawlerSource/spiders/Stage_One.py
@@ -300,7 +300,7 @@ class FirstLevelSpider(scrapy.Spider):
     def parse_by_letter(self, response):
         channel_top = response.css("a.channel-item__link::attr(href)").get()
         with open("channels.txt", "a+") as f:
-            f.write(channel_top)
+            f.write(channel_top + "\n")
         yield {channel_top: response.css("a.channel-item__link::text").get()}
 
 
