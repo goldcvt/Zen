708be6ef7d0cbaeb8d8f34c7cc279d40fba4211e
creepin-coder <gldct.smurf@gmail.com>
2020-10-12 16:50:16 +0300

    Enabled proxies
    Signed-off-by: creepin-coder <gldct.smurf@gmail.com>

--- ZenCrawlerSource/settings.py
+++ ZenCrawlerSource/settings.py
@@ -58,7 +58,7 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
     # 'ZenCrawlerSource.middlewares.IPNoRetryDownloaderMiddleware': 120,
     # 'ZenCrawlerSource.middlewares.IPTestDownloaderMiddleware': 120,
-    # 'ZenCrawlerSource.middlewares.ZencrawlersourceDownloaderMiddleware': 120,
+    'ZenCrawlerSource.middlewares.ZencrawlersourceDownloaderMiddleware': 120,
     # 'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
     # 'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 410,
 }
