ebc11cc92e9dc48d6cdeb2a7eaf0fa9c6e11ec88
goldcvt <thegoldcat5@gmail.com>
2020-11-11 18:39:17 +0300

    Accept-Language RU only (so html is proper)

--- ZenCrawlerSource/settings.py
+++ ZenCrawlerSource/settings.py
@@ -39,7 +39,7 @@ COOKIES_ENABLED = False  # just in case, y'know
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': '*',
+  'Accept-Language': 'ru-RU,ru;q=0.9',
 }
 
 # Enable or disable spider middlewares
