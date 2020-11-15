bb3347b93cb2a72a426154b65a4a3bfb42d0d670
goldcvt <thegoldcat5@gmail.com>
2020-11-15 19:57:32 +0300

    Rebuild pipeline

--- ZenCrawlerSource/pipelines.py
+++ ZenCrawlerSource/pipelines.py
@@ -2,7 +2,7 @@ from crawler_toolz import db_ops
 import datetime
 from psycopg2 import InterfaceError
 from scrapy import signals
-from ZenCrawlerSource.items import ChannelItem
+from ZenCrawlerSource.items import ChannelItem, ArticleItem
 
 
 # Define your item pipelines here
@@ -50,63 +50,70 @@ class ChannelPipeline:
         spider.logger.info(f"Closed connection with zen_copy: {self.conn}")
 
     def process_item(self, item, spider):
-        # if isinstance(item, ChannelItem):  # i'm probably paranoid
-        try:
-            spider.logger.info("ITEM IS IN PIPELINE, PORCESSING...")
-            channel_dict = item
-            del channel_dict["articles"]
-            del channel_dict["whether_crawled"]
+        if isinstance(item, ChannelItem):  # i'm probably paranoid
+            try:
+                spider.logger.info("CHANNEL ITEM IS IN PIPELINE, PROCESSING...")
+                if item["is_crawled"]:
+                    # doing necessary stuff, you know
+                    conn = self.conn
+                    cursor = conn.cursor()
 
-            if item["whether_crawled"]:
-                # doing necessary stuff, you know
-                conn = self.conn
-                cursor = conn.cursor()
+                    #updating channel
+                    request = "UPDATE channels SET"
+                    for key in item.keys():
+                        request += " {} = {}".format(key, item[key])
+                    request += " WHERE url = {}".format(item["url"])
 
-                # updating articles - needs reworking, some of these actually need updating but some need to ne written
-                for article in item["articles"]:
-                    article_dict = dict(vars(article))
-                    request = "UPDATE articles SET"
-                    for key in article_dict.keys():
-                        request += " {} = {}".format(key, article_dict[key])
-                    request += " WHERE channel_id = {};".format(item["whether_crawled"])
                     cursor.execute(request)
                     conn.commit()
+                    cursor.close()
 
-                #updating channel
-                request = "UPDATE channels SET"
-                for key in channel_dict.keys():
-                    request += " {} = {}".format(key, channel_dict[key])
-                request += " WHERE url = {}".format(item["url"])
+                else:
+                    db_ops.write_to_db(self.conn, "channels", **item)  # write_to_db (channel)
+
+                spider.logger.info("CHANNEL ITEM PROCESSED")
+
+            except InterfaceError:
+                spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
+                self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
+                self.process_item(item, spider)
+            except AttributeError:
+                spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
+                self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
+                self.process_item(item, spider)
+
+        elif isinstance(item, ArticleItem):
+            conn = self.conn
+            cursor = conn.cursor()
+            channel_url_array = (item["source_link"].split("?")[0]).split("/")
+            if 'id' in channel_url_array:
+                channel_url = 'https://zen.yandex.ru/id/' + channel_url_array[-2]
+            else:
+                channel_url = 'https://zen.yandex.ru/' + channel_url_array[-2]
+            channel_id = db_ops.read_from_db(conn, "channels", "channel_id", where="url={}".format(channel_url))[0][0]
+            quantity = len(db_ops.read_from_db(conn, "articles", "id", where="channel_url={}".format(channel_url)))
+
+            if channel_id and quantity == 5:
+                request = "UPDATE articles SET"
+                for key in item.keys():
+                    request += " {} = {}".format(key, item[key])
+                request += " WHERE channel_id = {};".format(channel_id)
 
                 cursor.execute(request)
                 conn.commit()
                 cursor.close()
+            elif channel_id:
+                db_ops.write_to_db(self.conn, "articles", **item, channel_id=channel_id)
 
+                # we probably don't need stuff below
+                # request = "UPDATE articles SET channel_id = {} WHERE channel_url = {}".format(channel_id, channel_url)
+                #
+                # cursor.execute(request)
+                # conn.commit()
+                # cursor.close()
             else:
-                db_ops.write_to_db(self.conn, "channels", **channel_dict)  # write_to_db (channel)
-                channel_id = db_ops.read_from_db(self.conn, "channels", "channel_id", where="url={}".format(
-                        item["url"]))[0][0]
+                db_ops.write_to_db(self.conn, "articles", **item, channel_url=channel_url)
 
-                # put articles into db
-                for article in item["articles"]:
-                    article_dict = dict(vars(article), channel_id=channel_id)
-                    db_ops.write_to_db(self.conn, "articles", **article_dict)  # TODO write_to_db - article
 
-            spider.logger.info("ITEM PROCESSED")
+        return item  # TODO CHANGE TO DELETION?
 
-            # if channel_dict:
-            #     spider.logger.info(channel_dict)
-            # if article_dict:
-            #     print(article_dict)
-            return item  # TODO CHANGE TO DELETION?
-
-        except InterfaceError:
-            spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
-            self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
-            self.process_item(item, spider)
-        except AttributeError:
-            spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
-            self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
-            self.process_item(item, spider)
-        # else:
-        #     return item
