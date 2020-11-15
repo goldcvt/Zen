6afa5ad100d25876e0910633b674d1b43ceaf1b4
goldcvt <thegoldcat5@gmail.com>
2020-11-15 20:14:24 +0300

    edited pipeline

--- ZenCrawlerSource/pipelines.py
+++ ZenCrawlerSource/pipelines.py
@@ -50,14 +50,14 @@ class ChannelPipeline:
         spider.logger.info(f"Closed connection with zen_copy: {self.conn}")
 
     def process_item(self, item, spider):
-        if isinstance(item, ChannelItem):  # i'm probably paranoid
-            try:
+        # doing necessary stuff, you know
+        conn = self.conn
+        cursor = conn.cursor()
+        try:
+            if isinstance(item, ChannelItem):  # i'm probably paranoid
+
                 spider.logger.info("CHANNEL ITEM IS IN PIPELINE, PROCESSING...")
                 if item["is_crawled"]:
-                    # doing necessary stuff, you know
-                    conn = self.conn
-                    cursor = conn.cursor()
-
                     #updating channel
                     request = "UPDATE channels SET"
                     for key in item.keys():
@@ -73,46 +73,48 @@ class ChannelPipeline:
 
                 spider.logger.info("CHANNEL ITEM PROCESSED")
 
-            except InterfaceError:
-                spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
-                self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
-                self.process_item(item, spider)
-            except AttributeError:
-                spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
-                self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
-                self.process_item(item, spider)
 
-        elif isinstance(item, ArticleItem):
-            conn = self.conn
-            cursor = conn.cursor()
-            channel_url_array = (item["source_link"].split("?")[0]).split("/")
-            if 'id' in channel_url_array:
-                channel_url = 'https://zen.yandex.ru/id/' + channel_url_array[-2]
-            else:
-                channel_url = 'https://zen.yandex.ru/' + channel_url_array[-2]
-            channel_id = db_ops.read_from_db(conn, "channels", "channel_id", where="url={}".format(channel_url))[0][0]
-            quantity = len(db_ops.read_from_db(conn, "articles", "id", where="channel_url={}".format(channel_url)))
 
-            if channel_id and quantity == 5:
-                request = "UPDATE articles SET channel_url = {}".format(channel_url)
-                for key in item.keys():
-                    request += " {} = {}".format(key, item[key])
-                request += " WHERE channel_id = {};".format(channel_id)
+            elif isinstance(item, ArticleItem):
+                spider.logger.info("ARTICLE ITEM IS IN PIPELINE, PROCESSING...")
+                channel_url_array = (item["source_link"].split("?")[0]).split("/")
+                if 'id' in channel_url_array:
+                    channel_url = 'https://zen.yandex.ru/id/' + channel_url_array[-2]
+                else:
+                    channel_url = 'https://zen.yandex.ru/' + channel_url_array[-2]
+                channel_id = db_ops.read_from_db(conn, "channels", "channel_id", where="url={}".format(channel_url))[0][0]
+                quantity = len(db_ops.read_from_db(conn, "articles", "id", where="channel_url={}".format(channel_url)))
 
-                cursor.execute(request)
-                conn.commit()
-                cursor.close()
-            elif channel_id:
-                db_ops.write_to_db(self.conn, "articles", **item, channel_id=channel_id, channel_url=channel_url)
+                if channel_id and quantity == 5:
+                    request = "UPDATE articles SET channel_url = {}".format(channel_url)
+                    for key in item.keys():
+                        request += " {} = {}".format(key, item[key])
+                    request += " WHERE channel_id = {};".format(channel_id)
 
-                # we probably don't need stuff below
-                # request = "UPDATE articles SET channel_id = {} WHERE channel_url = {}".format(channel_id, channel_url)
-                #
-                # cursor.execute(request)
-                # conn.commit()
-                # cursor.close()
-            else:
-                db_ops.write_to_db(self.conn, "articles", **item, channel_url=channel_url)
+                    cursor.execute(request)
+                    conn.commit()
+                    cursor.close()
+                elif channel_id:
+                    request = "UPDATE articles SET channel_id = {} WHERE channel_url = {}".format(channel_id, channel_url)
+
+                    cursor.execute(request)
+                    conn.commit()
+                    cursor.close()
+                    db_ops.write_to_db(self.conn, "articles", **item, channel_id=channel_id, channel_url=channel_url)
+
+                    # we probably don't need stuff below
+                else:
+                    db_ops.write_to_db(self.conn, "articles", **item, channel_url=channel_url)
+                spider.logger.info("ARTICLE ITEM PROCESSED")
+
+        except InterfaceError:
+            spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
+            self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
+            self.process_item(item, spider)
+        except AttributeError:
+            spider.logger.info("ITEM DB CONN FAILED, RE-ESTABLISHING")
+            self.conn = db_ops.connect_to_db(self.db, self.usr, self.pswd, self.hst)
+            self.process_item(item, spider)
 
 
         return item  # TODO CHANGE TO DELETION?
