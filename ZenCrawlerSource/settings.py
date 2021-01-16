fab50acce3dc743dcd56abd578213854413d42bf
creepin-coder <gldct.smurf@gmail.com>
2021-01-16 18:30:35 +0300

    Straight logging re-implemented

--- ZenCrawlerSource/settings.py
+++ ZenCrawlerSource/settings.py
@@ -64,7 +64,7 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 410,
 }
 
-LOG_FILE = "logs.txt"
+# LOG_FILE = "logs.txt"
 RANDOM_UA_PER_PROXY = False
 RANDOM_UA_TYPE = 'desktop.chrome'
 
