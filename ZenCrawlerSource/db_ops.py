7894476f9a4c0205ea06094206923ff1d3fc8f1f
goldcvt <thegoldcat5@gmail.com>
2021-10-11 21:24:35 +0300

    cleanup

--- ZenCrawlerSource/db_ops.py
+++ ZenCrawlerSource/db_ops.py
@@ -3,23 +3,16 @@ import psycopg2
 def del_from_db(connection, tablename, **kwargs):
 	cursor = connection.cursor()
 	conditions = "".join(" {}={}".format(k.lower(), v) for k,v in iter(kwargs.items()))
-	request = "DELETE FROM {} WHERE ({});".format(tablename, conditions) #возможно, нужны плейсхолдеры %s вместо {}
-	# print(cursor.mogrify(request))
-	# ЧИСТО ДЛЯ ТЕСТОВ
-	# if ask() == True:
+	request = "DELETE FROM {} WHERE ({});".format(tablename, conditions)
 	cursor.execute(request)
 	connection.commit()
 	cursor.close()
 
-def read_from_db(connection, tablename, *columns, **filters): #когда в вызове пихаешь аргументы в виде списка/словаря перед ними звездочки надо
-#но нахуй писать лишние словари?)
+def read_from_db(connection, tablename, *columns, **filters):
 	curs = connection.cursor()
 	filts = "".join(" {} {}".format(" ".join(k.split(sep="_")).upper(), str(v)) for k,v in iter(filters.items()))
 	cols = ",".join("{}".format(str(y)) for y in columns)
-	request = "SELECT ({}) FROM {}{};".format(cols, tablename, filts) #возможно, нужны плейсхолдеры %s вместо {}
-	# print(curs.mogrify(request))
-	# ЧИСТО ДЛЯ ТЕСТОВ, УДАЛИ ПОТОМ
-	# if ask() == True:
+	request = "SELECT ({}) FROM {}{};".format(cols, tablename, filts)
 	curs.execute(request)
 	connection.commit()
 	res = curs.fetchall()
@@ -30,16 +23,11 @@ def read_from_db(connection, tablename, *columns, **filters): #когда в в�
 def connect_to_db(basename, username, pswd, dbip):
 	connection = psycopg2.connect(dbname=basename, user=username, password=pswd, host=dbip)
 	return connection
-	#че, после закрытия соединения каждый божий раз придётся к базе подключаться?))
-	#да, поэтому не закрываем до победного конца))
 
 def write_to_db(connection, tablename, **kwargs):
 	cursor = connection.cursor()
 	request = "INSERT INTO {} ({}) VALUES ({});".format(tablename, ",".join(x for x in kwargs.keys()), 
 		",".join(str(x) for x in kwargs.values()))
-	# print(cursor.mogrify(request))
-	# ЧИСТО ДЛЯ ТЕСТОВ, УДАЛИ ПОТОМ
-	# if ask() == True:
 	cursor.execute(request)
 	connection.commit()
 	cursor.close()
